bd66b1f62f9a676f97c145e7af417282d36ed8c5
Johnsyisme <88381657+Johnsyisme@users.noreply.github.com>
2023-02-16 15:11:51 +0800

    Update run.sh

--- Lammps calls python/run.sh
+++ Lammps calls python/run.sh
@@ -1,28 +1,11 @@
 #!/bin/bash - 
-#===============================================================================
-#
-#          FILE: run.sh
-# 
-#         USAGE: ./run.sh 
-# 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 09/22/2022 16:47
-#      REVISION:  ---
-#===============================================================================
+
 module load lammps
 
 mkdir structure
 lmp -i in.lammps
 
 a=` tail -1 ./energy_MC.log | awk '{print $2}'`
-#a=` ls -l -r -t structure/step*| tail -1 | awk '{print $9}'`
 c=${a%%\,*}
 
 while [ $c -lt 200000 ]
